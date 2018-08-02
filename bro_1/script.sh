1a99f311cc82f736c7c7702a24562a05000d40d4
Marchi1993 <cys5322@gmail.com>
2018-08-02 16:27:01 +0900

    Delete Dummy

--- bro_1/script.sh
+++
@@ -1,4 +0,0 @@
-bro -C -r ../sample/ftpcrack.pcap test1.bro
-grep Bruteforcing notice.log > ftp_crack.log
-grep Bruteforcing notice.log | awk '{print $11 " is detected"}'
-grep Bruteforcing notice.log | awk '{print $12,$13,$14,$15,$16,$17,$18,$19,$20,$21,$22}'
